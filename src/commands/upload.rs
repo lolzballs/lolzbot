8c4221f85d9d703085d408328d008f56c6b3bb6a
Benjamin Cheng <ben@bcheng.cf>
2017-06-03 13:18:17 -0400

    Upload to imgur

--- src/commands/upload.rs
+++ src/commands/upload.rs
@@ -1,34 +1,53 @@
-use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::Cursor;
 
+use serde_json::{self, Value};
+use hyper::Client;
+use hyper::header::Authorization;
+use multipart::client::lazy::Multipart;
 use serenity::client::Context;
 use serenity::model::{Message, MessageId};
 
 pub const PREFIX: &'static str = "upload";
 
-pub fn handle(_: Context, msg: &Message, cmd: &str) -> ::Result<Option<MessageId>> {
+pub fn handle(ctx: Context, msg: &Message, cmd: &str) -> ::Result<Option<MessageId>> {
     match ::CONFIG.admins.iter().find(|&&a| a == msg.author.id.0) {
         Some(_) => {
             if msg.attachments.len() == 1 {
-                let mut file = match OpenOptions::new()
-                          .write(true)
-                          .create_new(true)
-                          .open([cmd, ".jpg"].concat()) {
-                    Ok(file) => file,
-                    Err(e) => {
-                        return Ok(Some(msg.reply(&format!("Error creating file: {}", e))?.id))
-                    }
-                };
-                let image = match msg.attachments[0].download() {
-                    Ok(image) => image,
+                let mut image = match msg.attachments[0].download() {
+                    Ok(image) => Cursor::new(image),
                     Err(e) => {
                         return Ok(Some(msg.reply(&format!("Error downloading image: {}", e))?
                                            .id))
                     }
                 };
-                match file.write_all(&image) {
-                    Ok(_) => Ok(Some(msg.reply("Image created!")?.id)),
-                    Err(e) => Ok(Some(msg.reply(&format!("Error writing to file: {}", e))?.id)),
+                let res = Multipart::new()
+                    .add_stream("image", &mut image, None as Option<&str>, None)
+                    .client_request_mut(&Client::new(), "https://api.imgur.com/3/image", |r| {
+                        r.header(Authorization(["Client-ID ", &::CONFIG.imgur_id].concat()))
+                    })?;
+                let res: Value = serde_json::from_reader(res)?;
+                match res["success"] {
+                    Value::Bool(true) => {
+                        match res["data"]["id"] {
+                            Value::String(ref code) => {
+                                let db = {
+                                    let data = match ctx.data.lock() {
+                                        Ok(data) => data,
+                                        Err(_) => bail!(::ErrorKind::MutexPosioned),
+                                    };
+                                    match data.get::<::DbPool>() {
+                                        Some(db) => db.clone(),
+                                        None => bail!(::ErrorKind::NoDatabase),
+                                    }
+                                };
+                                db.prep_exec("INSERT INTO images (`name`, `code`) VALUES (?, ?)",
+                                               (cmd, code))?;
+                                Ok(Some(msg.reply("Image uploaded!")?.id))
+                            }
+                            _ => Ok(Some(msg.reply("imgur did something weird!")?.id)),
+                        }
+                    }
+                    _ => Ok(Some(msg.reply("imgur failed!")?.id)),
                 }
             } else {
                 Ok(Some(msg.reply("You can only upload one image at a time")?.id))
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate serde_derive;
 extern crate mysql;
 
 extern crate hyper;
+extern crate multipart;
 extern crate serde;
 extern crate serde_json;
 extern crate serenity;
