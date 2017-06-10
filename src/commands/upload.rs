a8d7e140c42c46b60cab0ff75e53b13eae0d8625
Benjamin Cheng <ben@bcheng.cf>
2017-06-10 18:20:18 -0400

    Update upload and multipart

--- src/commands/upload.rs
+++ src/commands/upload.rs
@@ -4,6 +4,7 @@ use serde_json::{self, Value};
 use hyper::client::Client;
 use hyper::header::Authorization;
 use multipart::client::lazy::Multipart;
+use mysql;
 use serenity::client::Context;
 use serenity::model::Message;
 
@@ -23,7 +24,7 @@ pub fn handle(ctx: Context, msg: &Message, cmd: &str) -> super::CommandResult {
                 };
                 let res = Multipart::new()
                     .add_stream("image", &mut image, None as Option<&str>, None)
-                    .client_request_mut(&Client::new(), "https://api.imgur.com/3/image", |r| {
+                    .client_request_mut(&Client::new(), "http://api.imgur.com/3/image", |r| {
                         r.header(Authorization(["Client-ID ", &::CONFIG.imgur_id].concat()))
                     })?;
                 let res: Value = serde_json::from_reader(res)?;
@@ -32,9 +33,23 @@ pub fn handle(ctx: Context, msg: &Message, cmd: &str) -> super::CommandResult {
                         match res["data"]["id"] {
                             Value::String(ref code) => {
                                 let db = get_database!(ctx);
-                                db.prep_exec("INSERT INTO images (`name`, `code`) VALUES (?, ?)",
-                                               (cmd, code))?;
-                                Ok((Some(msg.reply("Image uploaded!")?.id), None))
+                                let res = db.prep_exec("INSERT INTO images (`name`, `code`) VALUES (?, ?)",
+                                                       (cmd, code));
+                                match res {
+                                    Ok(res) => {
+                                        if res.affected_rows() == 0 {
+                                            Ok((None, None))
+                                        } else {
+                                            Ok((Some(msg.reply("Image uploaded!")?.id), None))
+                                        }
+                                    }
+                                    Err(mysql::Error::MySqlError(mysql::MySqlError {
+                                                                     code: 1062, ..
+                                                                 })) => {
+                                        Ok((Some(msg.reply("Image has duplicate name")?.id), None))
+                                    }
+                                    Err(e) => Err(e.into()),
+                                }
                             }
                             _ => Ok((Some(msg.reply("imgur did something weird!")?.id), None)),
                         }
